07d76ff64b462c960775361ea3291b752a6e8baa
JK <78825154+kaue431@users.noreply.github.com>
2024-09-19 07:46:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ const musicFiles = [
     'Trap do Guilherme.mp3',
     'Tropa.mp3',
     'Tropa 2.0.mp3',
-    'Se essa Tropa fosse minha.mp3'
+    'Se essa Tropa fosse minha.mp3',
+    'Five Night At Aswra.mp3',
+    'Soberanos do Fim.mp3'
+    
 ];
 
 let currentIndex = 0;
